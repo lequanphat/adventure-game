4f84ecce29d5202c6046ec52a1eeb226fdb6c302
lequanphat <lequanphat3579@gmail.com>
2023-11-12 16:37:40 +0700

    hand detector

--- main.py
+++ main.py
@@ -60,6 +60,12 @@ from assets.assets import coin_img
 from assets.assets import exit_img2 
 
 
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
 
 pygame.mixer.pre_init(44100, -16, 2, 512)
 mixer.init()
@@ -73,6 +79,11 @@ setting_menu = False
 level = 1
 score = 0
 pedding=False
+up_action = False
+left_action = False
+right_action = False
+reset_up_action = False
+
 my_background = background[0]
 # init screen
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -125,7 +136,7 @@ def draw_grid():
 		#vertical lines
 		pygame.draw.line(screen, white, (c * tile_size, 0), (c * tile_size, screen_height - margin))
 		#horizontal lines
-		pygame.draw.line(screen, white, (0, c * tile_size ), (screen_width-200, c * tile_size ))
+		pygame.draw.line(screen, white, (0, c * tile_size ), (screen_width-margin, c * tile_size ))
 
 
 def draw_world():
@@ -213,7 +224,7 @@ class Player():
 			#get keypresses
 			key = pygame.key.get_pressed()
 			if pedding == False:
-
+				# key event
 				if key[pygame.K_SPACE] and self.jumped == False  and self.in_air == False:
 					jump_fx.play()
 					self.vel_y = -15
@@ -238,6 +249,33 @@ class Player():
 						self.image = self.images_right[self.index]
 					if self.direction == -1:
 						self.image = self.images_left[self.index]
+				# hand event
+				if up_action == True and self.jumped == False  and self.in_air == False:
+					jump_fx.play()
+					self.vel_y = -15
+					self.jumped = True
+
+				if up_action == False:
+					self.jumped = False
+
+				if left_action == True:
+					dx -= 5
+					self.counter += 1
+					self.direction = -1
+
+				if right_action == True:
+					dx += 5
+					self.counter += 1
+					self.direction = 1
+
+				if left_action == False and right_action == False:
+					self.counter = 0
+					self.index = 0
+					if self.direction == 1:
+						self.image = self.images_right[self.index]
+					if self.direction == -1:
+						self.image = self.images_left[self.index]
+
 
 			#handle animation
 			if self.counter > walk_cooldown:
@@ -353,177 +391,243 @@ class Player():
 world_data = load_world_data()
 world = World(screen, world_data, blob_group, platform_group, lava_group, coin_group, exit_group)
 player = Player(screen, 100, screen_height - 130)
+# capture
+cap = cv2.VideoCapture(0)
 
-while run:
-	clock.tick(fps) 
-	screen.blit(menu_background, (0, 0))
+# run 
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
 	
-	# screen.blit(sun_img, (100, 100))
+	while run:
 
-	if main_menu == True:
-		#draw logo
-		screen.blit(logo, (screen_width // 2 - 200,20))
-		# handle logic
-		if exit_button.draw():
-			run = False
-		if start_button.draw():
-			main_menu = False
-		if setting_button.draw():
-			setting_menu = True
-			main_menu = False
-			tile_size = 30
-	elif setting_menu == True:
-		#draw background
-		screen.fill(green)
-		screen.blit(setting_background, (0,0))
-		#load and save level
-		if save_button.draw():
-			#save level data
-			pickle_out = open(f'./env/level{level}_data', 'wb')
-			pickle.dump(world_data, pickle_out)
-			pickle_out.close()
-			world = reset_level(player, level)
-			print('Save data here')
-		if load_button.draw():
-			#load in level data
-			world_data = load_world_data()
-			if world_data == None:
-				world_data = generate_new_world()
-		if back_button.draw():
-			setting_menu = False
-			main_menu = True
+		success, image = cap.read()
+		if not success:
+			print("Ignoring empty camera frame.")
+			continue
+			
+		image.flags.writeable = False
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		results = hands.process(image)
+		image.flags.writeable = True
+		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+		if results.multi_hand_landmarks:
+			for hand_landmarks in results.multi_hand_landmarks:
+				mp_drawing.draw_landmarks(
+					image,
+					hand_landmarks,
+					mp_hands.HAND_CONNECTIONS,
+					mp_drawing_styles.get_default_hand_landmarks_style(),
+					mp_drawing_styles.get_default_hand_connections_style())
+			
+			for hand_landmarks in results.multi_hand_landmarks:
+				handList = []
+				for landmark in hand_landmarks.landmark:
+					x = landmark.x
+					y = landmark.y
+					z = landmark.z
+					handList.append([x,y,z])
+				if (handList[6][1] - handList[8][1])*1000 > 70:
+					if reset_up_action == True:
+						up_action = True
+						reset_up_action = False
+						print('up_action: '+str(up_action))
+					else:
+						up_action = False
+					
+				if handList[8][1] > handList[6][1]:
+					reset_up_action = True
+					up_action = False
+				if (handList[18][1] - handList[20][1])*1000 > 80:
+					right_action = True
+					print('left_action: '+str(right_action))
+					
+				if (handList[4][0] - handList[2][0])*1000 > 70:
+					left_action = True
+					print('right_action: '+str(left_action))
+						
+				# reset
+				if handList[20][1] > handList[18][1]:
+					right_action = False
+				if handList[2][0] > handList[4][0]:
+					left_action = False
+		cv2.imshow('Hand detector', cv2.flip(image, 1))
+		if cv2.waitKey(1) == ord('q'):
+			break
+		# ---
+		clock.tick(fps) 
+		screen.blit(menu_background, (0, 0))
 		
-		#show the grid and draw the level tiles
-		draw_grid()
-		draw_world()
+		# screen.blit(sun_img, (100, 100))
 
-		#text showing current level
-		draw_text(f'Level: {level}', pygame.font.SysFont('Futura', 32), white, 100, screen_height - 140)
-		draw_text('Press UP or DOWN to change level', pygame.font.SysFont('Futura', 32), white, 100, screen_height - 110)
-
-		#event handler
-		for event in pygame.event.get():
-			#quit game
-			if event.type == pygame.QUIT:
+		if main_menu == True:
+			#draw logo
+			screen.blit(logo, (screen_width // 2 - 200,20))
+			# handle logic
+			if exit_button.draw():
 				run = False
-			#mouseclicks to change tiles
-
-			if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
-				clicked = True
-				pos = pygame.mouse.get_pos()
-				x = pos[0] // tile_size
-				y = pos[1] // tile_size
-				#check that the coordinates are within the tile area
-				if x < 20 and y < 20:
-					#update tile value
-					if pygame.mouse.get_pressed()[0] == 1:
-						world_data[y][x] += 1
-						if world_data[y][x] > 8:
-							world_data[y][x] = 0
-					elif pygame.mouse.get_pressed()[2] == 1:
-						world_data[y][x] -= 1
-						if world_data[y][x] < 0:
-							world_data[y][x] = 8
-							
-			if event.type == pygame.MOUSEBUTTONUP:
-				clicked = False
-			#up and down key presses to change level number
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_UP:
-					level += 1
-				elif event.key == pygame.K_DOWN and level > 1:
-					level -= 1
-		#update game display window
-		pygame.display.update()
-
-
-		# end
-	else:
-		screen.blit(my_background, (0, 0))
-		world.draw()
-		if game_over == 0:
-			blob_group.update()
-			platform_group.update()
-			#update score
-			#check if a coin has been collected
-
-			if pygame.sprite.spritecollide(player, coin_group, True):
-				score += 1
-				coin_fx.play()
-			draw_text('X ' + str(score), font_score, white, tile_size , 6)
-		
-		blob_group.draw(screen)
-		platform_group.draw(screen)
-		lava_group.draw(screen)
-		coin_group.draw(screen)
-		exit_group.draw(screen)
-		game_over = player.update(game_over)
-		# if you click ESC
-
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				pedding = True
-
-		# pedding state
-
-		if pedding==True:
-			if restart_button.draw():
-				pedding=False
-			if menu_button.draw():
-				world_data = load_world_data()
-				world = reset_level(player,  level)
-				game_over = 0
-				score = 0
-				main_menu = True
-				pedding=False
-
-		#if player has died
-
-		if game_over == -1:
-			if restart_button.draw():
-				world_data = []
-				world = reset_level(player,  level)
-				game_over = 0
-				score = 0
-			if menu_button.draw():
-				world_data = load_world_data()
-				world = reset_level(player,  level)
-				game_over = 0
-				score = 0
-				main_menu = True
-
-		#if player has completed the level
-		if game_over == 1:
-			level += 1
-
-			if load_world_data() != None:
-				my_background = load_background()
-				world_data = []
+			if start_button.draw():
+				main_menu = False
+			if setting_button.draw():
+				setting_menu = True
+				main_menu = False
+				tile_size = 30
+				
+		elif setting_menu == True:
+			#draw background
+			screen.fill(green)
+			screen.blit(setting_background, (0,0))
+			#load and save level
+			if save_button.draw():
+				#save level data
+				pickle_out = open(f'./env/level{level}_data', 'wb')
+				pickle.dump(world_data, pickle_out)
+				pickle_out.close()
 				world = reset_level(player, level)
-				game_over = 0
+				print('Save data here')
+			if load_button.draw():
+				#load in level data
+				world_data = load_world_data()
+				if world_data == None:
+					world_data = generate_new_world()
+			if back_button.draw():
+				setting_menu = False
+				main_menu = True
+			
+			#show the grid and draw the level tiles
+			draw_grid()
+			draw_world()
 
-			else:
-				draw_text('YOU WIN!', font, yellow, (screen_width // 2) - 140, screen_height // 2 - 215)
+			#text showing current level
+			draw_text(f'Level: {level}', pygame.font.SysFont('Futura', 32), white, 100, screen_height - 140)
+			draw_text('Press UP or DOWN to change level', pygame.font.SysFont('Futura', 32), white, 100, screen_height - 110)
+
+			#event handler
+			for event in pygame.event.get():
+				#quit game
+				if event.type == pygame.QUIT:
+					run = False
+				#mouseclicks to change tiles
+
+				if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+					clicked = True
+					pos = pygame.mouse.get_pos()
+					x = pos[0] // tile_size
+					y = pos[1] // tile_size
+					#check that the coordinates are within the tile area
+					if x < 20 and y < 20:
+						#update tile value
+						if pygame.mouse.get_pressed()[0] == 1:
+							world_data[y][x] += 1
+							if world_data[y][x] > 8:
+								world_data[y][x] = 0
+						elif pygame.mouse.get_pressed()[2] == 1:
+							world_data[y][x] -= 1
+							if world_data[y][x] < 0:
+								world_data[y][x] = 8
+								
+				if event.type == pygame.MOUSEBUTTONUP:
+					clicked = False
+				#up and down key presses to change level number
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_UP:
+						level += 1
+					elif event.key == pygame.K_DOWN and level > 1:
+						level -= 1
+			#update game display window
+			pygame.display.update()
+
+
+			# end
+		else:
+			screen.blit(my_background, (0, 0))
+			world.draw()
+			if game_over == 0:
+				blob_group.update()
+				platform_group.update()
+				#update score
+				#check if a coin has been collected
+
+				if pygame.sprite.spritecollide(player, coin_group, True):
+					score += 1
+					coin_fx.play()
+				draw_text('X ' + str(score), font_score, white, tile_size , 6)
+			
+			blob_group.draw(screen)
+			platform_group.draw(screen)
+			lava_group.draw(screen)
+			coin_group.draw(screen)
+			exit_group.draw(screen)
+			game_over = player.update(game_over)
+			# if you click ESC
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					pedding = True
+
+			# pedding state
+
+			if pedding==True:
 				if restart_button.draw():
-					level = 1
-					#reset level
+					pedding=False
+				if menu_button.draw():
+					world_data = load_world_data()
+					world = reset_level(player,  level)
+					game_over = 0
+					score = 0
+					main_menu = True
+					pedding=False
+
+			#if player has died
+
+			if game_over == -1:
+				if restart_button.draw():
 					world_data = []
-					world = reset_level(player, level)
+					world = reset_level(player,  level)
 					game_over = 0
 					score = 0
 				if menu_button.draw():
-					level = 1
-					#reset level
 					world_data = load_world_data()
-					world = reset_level(player, level)
-					main_menu = True
+					world = reset_level(player,  level)
 					game_over = 0
 					score = 0
+					main_menu = True
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
+			#if player has completed the level
+			if game_over == 1:
+				level += 1
 
-	pygame.display.update()
+				if load_world_data() != None:
+					my_background = load_background()
+					world_data = []
+					world = reset_level(player, level)
+					game_over = 0
+
+				else:
+					draw_text('YOU WIN!', font, yellow, (screen_width // 2) - 140, screen_height // 2 - 215)
+					if restart_button.draw():
+						level = 1
+						#reset level
+						world_data = []
+						world = reset_level(player, level)
+						game_over = 0
+						score = 0
+					if menu_button.draw():
+						level = 1
+						#reset level
+						world_data = load_world_data()
+						world = reset_level(player, level)
+						main_menu = True
+						game_over = 0
+						score = 0
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+		pygame.display.update()
 
 pygame.quit()
+cap.release()
+cv2.destroyAllWindows()
